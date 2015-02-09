f102a579d72657a1596ed2fdf083b6c3f5b49d7a
Nick Ray <nickray22@yahoo.com>
2015-02-09 04:12:48 -0500

    Etsy Site done, needs some polishing

---
+++ app/controllers/etsy_controller.rb
@@ -0,0 +1,12 @@
+class EtsyController < ApplicationController
+ 
+  def search
+    if params[:q]
+      response = Etsy::Request.get('/listings/active',
+                                   :includes => ['Images', 'Shop'],
+                                   :keywords => params[:q])
+      @results = JSON.parse(response.body)['results']
+    end
+    render :search
+  end
+end
